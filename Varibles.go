866951501e5463fae108e7d97c68feb037423382
Sam Hedges <106534363+HedgeHogNo5@users.noreply.github.com>
2022-12-09 22:00:00 +0000

    This is practicing messing with varibles and how they can be assigned a value

---
+++ Varibles.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var x int
+	x = 5         // this is the first way you can create a varible. It is defined as an integer
+	var y int = 7 // this is teh second way you can make an
+	z := 9        // this is the final way you can create a variable,
+	sum := x + y + z
+	fmt.Println(sum)
+}
