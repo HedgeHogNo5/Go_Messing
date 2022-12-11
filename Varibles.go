42704e1b9d8d455ee7cd337774ab23aec4324170
Sam Hedges <106534363+HedgeHogNo5@users.noreply.github.com>
2022-12-11 20:58:57 +0000

    Chanegd main to sum to stop the double use of main and added a new function called avrg_3 which takes three float input x, y, z and outputs the functions of teh three

--- Varibles.go
+++ Varibles.go
@@ -4,11 +4,15 @@ import (
 	"fmt"
 )
 
-func main() {
+func Sum() {
 	var x int
 	x = 5         // this is the first way you can create a varible. It is defined as an integer
 	var y int = 7 // this is teh second way you can make an
 	z := 9        // this is the final way you can create a variable,
 	sum := x + y + z
 	fmt.Println(sum)
+}
+
+func Avrg_3(x float64, y float64, z float64) float64 {
+	return(x + y + z) / 3
 }
