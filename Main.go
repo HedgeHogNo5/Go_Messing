3318dcf5275e7fef4157bdf36b9bc0c587734862
Sam Hedges <106534363+HedgeHogNo5@users.noreply.github.com>
2022-12-24 11:17:07 +0000

    main.go now should work as intened main.go has been sucessful at finding and running functions described in other files

--- Main.go
+++ Main.go
@@ -1,5 +1,12 @@
 package main
 
+import "fmt"
+
 func main() {
-	Hello_World()
+	helloWorld()
+	sum()
+	fmt.Println(avrgThree(5, 5, 5))
+	xs := []float64{98, 93, 77, 82, 83}
+	fmt.Println(avrgN(xs))
+	inputPrac1()
 }
