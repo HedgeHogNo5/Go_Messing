4fe5e5fb2106b125edadbd8a4d711f22f9d78d57
Sam Hedges <106534363+HedgeHogNo5@users.noreply.github.com>
2022-12-24 11:18:02 +0000

    Created avrgN, added empty string case avrgN takes an array and passes it through averaging the array by it'd length In the case of empty string both AvrgN and avrgThree should now output 0 in line with best practises

--- Varibles.go
+++ Varibles.go
@@ -4,19 +4,33 @@ import (
 	"fmt"
 )
 
-func Sum() {
+func sum() {
 	var x int
 	x = 5         // this is the first way you can create a varible. It is defined as an integer
 	var y int = 7 // this is teh second way you can make an
 	z := 9        // this is the final way you can create a variable,
 	sum := x + y + z
 	fmt.Println(sum)
+	//a varible can also be defined in the code i.e. float64(var) would make var a float.
 }
 
-func Avrg_3(x float64, y float64, z float64) float64 {
-	return(x + y + z) / 3 //This takes in the float values of x,y,z and will output the average of the three 
+func avrgThree(x float64, y float64, z float64) float64 {
+	if x+y+z == 0 {
+		return 0
+	}
+	return (x + y + z) / 3 //This takes in the float values of x,y,z and will output the average of the three
 }
 
-func Avrg_n() {
-fmt.Println("What numbers are you averaging?")	
+func avrgN(xs []float64) float64 {
+	if len(xs) == 0 {
+		return 0
+	}
+	sum := 0.0
+	for _, v := range xs {
+		sum += v
+	}
+	return sum / float64(len(xs))
 }
+
+//This average takes in an array and outputs the average
+//If the array is empty it outputs 0
